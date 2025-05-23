1531f8bbfe2702ac9d0bae29a07f543af1fc4205
Jahidur Rahman Mahim <jahidurmahim@gmail.com>
2025-05-24 00:54:44 +0600

    CF_219774E.cpp

---
+++ CF_219774E.cpp
@@ -0,0 +1,34 @@
+						//     Bismillahir Rahmanir Rahim     //
+                       //        Jahidur Rahman Mahim        //
+                      //        AUTHOR : jahidurmhaim       //
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	int arr[t];
+	for(int i=0;i<t;i++) cin>>arr[i];
+	int temp=arr[0], m=0;
+	for(int i=1;i<t;i++){
+		if(arr[i]<temp)
+		{
+			temp= arr[i];
+			m=i;
+		}
+	}
+	cout<<temp<<" "<<m+1;
+}
+
+//Problem link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/E
+
+
+
+
+
+
+
+
+
+
+
+
