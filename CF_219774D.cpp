6741366f01a223c6a32d61cd62a1202f0d43fe33
Jahidur Rahman Mahim <jahidurmahim@gmail.com>
2025-05-23 23:53:55 +0600

    CF_219774D.cpp

---
+++ CF_219774D.cpp
@@ -0,0 +1,23 @@
+//     Bismillahir Rahmanir Rahim     //
+                       //        Jahidur Rahman Mahim        //
+                      //        AUTHOR : jahidurmhaim       //
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	int arr[t];
+	for(int i=0;i<t;i++) cin>>arr[i];
+	for(int i=0;i<t;i++){
+		if(arr[i]<=10) cout<<"A["<<i<<"] = "<<arr[i]<<endl;
+	}
+}
+//Problem link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/D
+
+
+
+
+
+
+
+
