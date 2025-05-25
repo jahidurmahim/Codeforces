51989c1160b52a7b5cc0c09ee66e28eccb5c92f7
Jahidur Rahman Mahim <jahidurmahim@gmail.com>
2025-05-26 01:09:18 +0600

    CF_2110B.cpp

---
+++ CF_2110B.cpp
@@ -0,0 +1,38 @@
+						                                          //     Bismillahir Rahmanir Rahim     //
+                                                     //        Jahidur Rahman Mahim        //
+                                                    //        AUTHOR : jahidurmhaim       //
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        stack<char>st;
+        int cnt=0;
+        for(int i=0;i<s.size();i++){
+            char c=s[i];
+            if(!st.empty() && st.top()=='(' && c== ')') st.pop();
+            else st.push(c);
+            if (st.empty()) cnt++;
+        }
+        if (cnt>1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
+
+//Problem link: https://codeforces.com/contest/2110/problem/B
+
+
+
+
+
+
+
+
+
+
+
+
