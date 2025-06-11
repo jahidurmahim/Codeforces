2d892c6de2b22dae39aca858aed7f2ea39638537
Jahidur Rahman Mahim <jahidurmahim@gmail.com>
2025-06-11 20:16:43 +0600

    CF_2117B.cpp

---
+++ CF_2117B.cpp
@@ -0,0 +1,18 @@
+                                                    //     Bismillahir Rahmanir Rahim     //
+                                                   //        Jahidur Rahman Mahim        //
+                                                  //        AUTHOR : jahidurmhaim       //
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int t,n;
+ cin>>t;
+ while(t--){
+  cin>>n;
+  cout<<1<<' ';
+  for(int i=n;i>=2;i--) cout<<i<<' ';
+  cout<<endl;
+ }
+ return 0;
+}
+
+//Problam Link: https://codeforces.com/contest/2117/problem/B
