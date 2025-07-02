614073a5c4263d967b9b7818029d07a19f24759e
Jahidur Rahman Mahim <jahidurmahim@gmail.com>
2025-07-03 00:18:27 +0600

    CF_2123A.cpp

---
+++ CF_2123A.cpp
@@ -0,0 +1,16 @@
+                                                    //     Bismillahir Rahmanir Rahim     //
+                                                   //        Jahidur Rahman Mahim        //
+                                                  //        AUTHOR : jahidurmhaim       //
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        if(n%4==0) cout<<"Bob"<<endl;
+        else cout<<"Alice"<<endl;
+    }
+}
+
+//Problam link: https://codeforces.com/contest/2123/problem/A
